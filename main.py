712feb97d5e88b042d3d225623a152d94581f7a1
Nathaniel <nthw.dev@gmail.com>
2022-06-24 15:06:48 -0400

    finished game for now
    Still having some scoreboard refresh issues but otherwise game is working as intended

--- main.py
+++ main.py
@@ -6,9 +6,34 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("not_Frogger")
 screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+screen.listen()
+screen.onkey(player.move, "Up")
+score = Scoreboard()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.spawn_car()
+    car_manager.drive_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+
+    if player.ycor() > 280:
+        score.level += 1
+        score.update_level()
+        car_manager.speed_up()
+
+        player.start()
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,24 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.penup()
+        self.color("purple")
+        self.ht()
+        self.goto(x=-200, y=265)
+        self.update_level()
+
+    def clear_screen(self):
+        self.clear()
+
+    def update_level(self):
+        self.clear_screen()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT )
